6dd349db2bf2b7676d0985cdfebee62b13d0770d
Sergey Muravskiy <0717376@gmail.com>
2022-12-06 21:24:57 +0300

    test commit

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.errorhandler(404)
+def error_404(e):
+    return '<h1>No source like your request.</h1>', 404
+
+@app.route('/')
+def index():
+    return "Hello world"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+     
